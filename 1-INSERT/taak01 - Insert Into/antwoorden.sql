e64056b0334ff8fb3ebb81099e72f7c2997a8dd4
pascalcreator <psclkon@gmail.com>
2021-02-07 23:38:05 +0100

    opdracht 1 en 2 sql adv

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,5 @@
+INSERT INTO `studenten` (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) VALUES
+(5, 'Henk', 'Bros', '9A', 'Amstelhoven 7', '1181PA', '1998-9-11', 'Amstelveen'),
+(6, 'Sanne', 'da Vince', '9A', 'Borssenburg 4', '1181NV', '2003-03-27', 'Amstelveen'),
+(7, 'Lucas', 'de Jong', '9A', 'Flevolaan 107', '1181GB', '2002-07-20', 'Amstelveen'),
 
--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO `studenten` (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) VALUES
+(8, 'Jan', 'Willem', '9A', 'Brantwijk 60', '1181MV', '2000-10-7', 'Amstelveen'),
+(9, 'Elise', 'Bos', '9A', 'Welna 7', '1181NS', '2001-07-21', 'Amstelveen'),
+(10, 'Stan', 'Vos', '9A', 'Jan Huddestraat 28', '1181SH', '2002-06-26', 'Amstelveen')
